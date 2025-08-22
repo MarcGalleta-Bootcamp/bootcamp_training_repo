0b6602b1b2106c76952dcedc9b0bf405c31f06a8
Marc Anjelou Galleta <marc.anjelou.galleta@accenture.com>
2025-08-22 05:13:51 +0000

    Removed comments and spaces

--- DAY 7/odata/webapp/controller/Main.controller.js
+++ DAY 7/odata/webapp/controller/Main.controller.js
@@ -7,16 +7,6 @@ sap.ui.define([
 
     return Controller.extend("sapips.training.odata.controller.Main", {
         onInit() {
-            // var oModel = new ODataModel("https://services.odata.org/V2/Northwind/Northwind.svc/Products");
-            // this.getView().setModel(oModel);
         },
-
-        // SortProdName: function () {
-            // var oTable = this.byId("tbl_products");
-            // oBinding = oTable.getBinding("items");
-
-            // var oSorter = new Sorter("ProductName", false);
-            // oBinding.sort(oSorter);
-        // }
     });
 });
